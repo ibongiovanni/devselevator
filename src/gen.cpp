fa2ec3da05c460304e855bf481c5ce37c59173a0
ibongiovanni <ignaciobongiovanni@gmail.com>
2016-06-23 14:44:56 -0300

    add generator DEVS

---
+++ src/gen.cpp
@@ -0,0 +1,49 @@
+#include "gen.h"
+void gen::init(double t,...) {
+//The 'parameters' variable contains the parameters transferred from the editor.
+va_list parameters;
+va_start(parameters,t);
+//To get a parameter: %Name% = va_arg(parameters,%Type%)
+//where:
+//      %Name% is the parameter name
+//	%Type% is the parameter type
+
+char *fvar= va_arg(parameters,char*); 
+lam=getScilabVar(fvar );
+fvar= va_arg(parameters,char*);
+int seed = (strlen(fvar)==0 ? (int)time(0)+rand() : getScilabVar(fvar)); // random seed
+s=0;
+for (int i=0;i<10;i++)y[i]=0;
+RanGen = new CRandomMersenne(seed);
+v= RanGen->IRandom(1,10);
+}
+double gen::ta(double t) {
+//This function returns a double.
+return s;
+}
+void gen::dint(double t) {
+s=-(1/lam)*log(1-RanGen->Random());
+}
+void gen::dext(Event x, double t) {
+//The input event is in the 'x' variable.
+//where:
+//     'x.value' is the value (pointer to void)
+//     'x.port' is the port number
+//     'e' is the time elapsed since last transition
+
+}
+Event gen::lambda(double t) {
+//This function returns an Event:
+//     Event(%&Value%, %NroPort%)
+//where:
+//     %&Value% points to the variable which contains the value.
+//     %NroPort% is the port number (from 0 to n-1)
+
+v= RanGen->IRandom(1,10);
+y[0]=v; 
+return Event(&y,0);
+}
+void gen::exit() {
+//Code executed at the end of the simulation.
+
+}
---
+++ src/gen.h
@@ -0,0 +1,46 @@
+//LIB:-lm
+//CPP:random/lib/randomc.cpp
+//CPP:userdevs/elevatordevs/src/gen.cpp
+#if !defined gen_h
+#define gen_h
+
+#include "simulator.h"
+#include "event.h"
+#include "stdarg.h"
+
+#include "time.h"
+#include "random/lib/randomc.h"
+#include "math.h"
+
+
+class gen: public Simulator { 
+// Declare the state,
+// output variables
+// and parameters
+
+//state
+double s;
+
+//output
+double v;
+
+//aux
+CRandomMersenne *RanGen;
+double y[10];
+
+//Params
+double lam;
+
+
+
+
+public:
+	gen(const char *n): Simulator(n) {};
+	void init(double, ...);
+	double ta(double t);
+	void dint(double);
+	void dext(Event , double );
+	Event lambda(double);
+	void exit();
+};
+#endif
