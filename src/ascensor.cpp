eb60b64bfcc327204c7dbd2db371edec81ef64b3
ibongiovanni <ignaciobongiovanni@gmail.com>
2016-12-02 13:01:48 -0300

    Update ascensor lambda and dint

--- src/ascensor.cpp
+++ src/ascensor.cpp
@@ -26,8 +26,8 @@ void ascensor::dint(double t) {
 printLog("(-- dint ASCENSOR \t\t\t");
 printLog("t= %2.2f\t",t);
 switch(est){
-	case 0: { h = nf(h); sigma = vc*altPiso; printLog("ASCENSOR: voy a seguir subiendo"); break; }
-	case 1: { h = pf(h); sigma = vc*altPiso; printLog("ASCENSOR: voy a seguir bajando"); break;}
+	case 0: { h = h + vc*sigma; sigma = vc*altPiso; printLog("ASCENSOR: voy a seguir subiendo"); break; }
+	case 1: { h = h - vc*sigma; sigma = vc*altPiso; printLog("ASCENSOR: voy a seguir bajando"); break;}
 	case 5: { est = 2; sigma = INF; printLog("ASCENSOR: voy a descansar"); break;}
 	case 6: { est = 2; sigma = INF; printLog("ASCENSOR: descansar"); break;}
 }
@@ -84,10 +84,10 @@ printLog("(-- lambda ASCENSOR \t\t");//This function returns an Event:
 //where:
 //     %&Value% points to the variable which contains the value.
 //     %NroPort% is the port number (from 0 to n-1)
-if ( est==0 || est==5) {value=nf(h)/2+1;}
-else {value=pf(h)/2+1;}
+if ( est==0 || est==5) {value=(h + vc*sigma)/2+1;}
+else {value=(h - vc*sigma)/2+1;}
 printLog("t= %2.2f\t",t);
-printLog("ASCENSOR: llegue al piso= %d, h: %2.2f ",value,h);
+printLog("ASCENSOR: llegue al piso= %d, h: %2.2f, est= %d ",value,h,est);
 printLog("\t --) \n");
 return Event(&value,0);
 
